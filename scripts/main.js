18b2083dabdf7b620ae99e1b7e9f1934dc31b4b6
Mollie Goforth <molliegoforth818@gmail.com>
2025-08-02 12:19:22 -0500

    changed to a h2

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ const container = document.querySelector("#container")
 
 const page = `
 <h1> Macho Moods <h1>
-<h3> How are you feeling today? <h3>
+<h2> How are you feeling today? <h2>
 `
 container.innerHTML = page
