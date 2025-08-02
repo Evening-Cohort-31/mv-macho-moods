26ad7d1cdd915f01a4b428a336ec0f8c2ea11310
Mollie Goforth <molliegoforth818@gmail.com>
2025-08-02 12:03:27 -0500

    added title

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 const container = document.querySelector("#container")
 
-const page = ``
-
+const page = `
+<h1> Macho Moods <h1>
+<h3> How are you feeling today? <h3>
+`
+container.innerHTML = page
